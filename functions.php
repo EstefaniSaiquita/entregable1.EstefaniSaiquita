a3923b63947a4b26a7c14192cc5901ab4ebda7e1
EstefaniSaiquita <naomi.tefi@gmail.com>
2024-05-21 18:34:55 -0300

    ejercicio 4 hecho

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
-echo "EJERCICIO UNO <br>";
 //llamo a la funcion y le doy un valor
-ValorAleatorio(rand(1,100));
+//ValorAleatorio(rand(1,100));
 //ejecutamos la funcion
 function ValorAleatorio($Nro){
     echo "$Nro<br>";
@@ -16,10 +15,9 @@ function ValorAleatorio($Nro){
         echo "El numero es igual a 50";
     }
 }
-echo "<br>";
-echo "<br> EJERCICIO DOS";
+
 // llamo la funcion 
-MesesAleatorios(rand(1,12));
+// MesesAleatorios(rand(1,12));
 // la variable mes tiene de valor un rand con 12 numeros 
 function MesesAleatorios($mes){
     // creo un arreglo asociativo con los valores del mes 
@@ -40,26 +38,99 @@ function MesesAleatorios($mes){
     echo "<br>$mes es igual a $Conjuntomes[$mes]";
 }
 
-echo "<br>";
-echo "<br> EJERCICIO TRES";
+
 // llamo la funcion q tiene 2 valores de las variables
-DosValores(rand(1,20), rand(1,20));
+//DosValores(rand(1,100), rand(1,100));
 function DosValores ($valor1, $valor2){
     echo " <br> valor uno: $valor1";
     echo "<br> valor dos: $valor2 <br>";
-
+//si el primer valor es menor al segundo valor que se haga contador hasta que el primero valga lo mismo que el segundo
     if ($valor1 < $valor2) {
-        for ($i=$valor1; $i < $valor2 ; $i++) { 
+        for ($i=($valor1+1); $i < $valor2 ; $i++) { 
+            echo "$i,";
+            
+        }
+        echo "ascendente <br> ";
+    } 
+//si el primero es mas grande que el segundo se va a restar
+    elseif ($valor1 > $valor2) {
+        for ($a=($valor1-1); $a > $valor2 ; $a--) { 
+            echo "$a,";
+        }
+        echo "descendente <br>";
+    }
+//es lo mismo que el primer condicional solo que con el segundo valor
+    if ($valor2 < $valor1) {
+        for ($i=($valor2 +1); $i < $valor1 ; $i++) { 
             echo "$i,";
         }
         echo "ascendente <br> ";
-    } elseif ($valor1 > $valor2) {
-        for ($a=$valor1; $a > $valor2 ; $a--) { 
+    } elseif ($valor2 > $valor1) {
+        for ($a=($valor2 -1); $a > $valor1 ; $a--) { 
             echo "$a,";
         }
         echo "descendente <br>";
     }
 }
+// el primer valor vale 2 y en el segundo hay un random 
+function PrimeraTabla ($num, $limite){
+    echo "<br> num:$num limite: $limite";
+// i vale 0 y si i es menor e igual a a la multiplicacion del 2 por el limite, entonces guardamos el valor de i y lo vamos sumando el valor de num (un acumulador)
+    for ($i= 0; $i <= ($num * $limite) ; $i = $i + $num) { 
+        echo "<br> $i";
+    }
+}
 
-echo "<br>";
-echo "<br> EJERCICIO CUATRO";
+function Segunda ($num, $limite){
+    echo "numero: $num limite: $limite <br>";
+    $i = 0;
+    do {
+        echo $i = $i + $num;
+        echo "<br>";
+    } while ($i <= ($num * $limite)- $num);
+}
+
+function Tercera ($num, $limite){
+    echo "numero: $num limite: $limite <br>";
+    $i = 0;
+    while (($i <= ($num * $limite)- $num)) {
+        echo $i = $i + $num;
+        echo "<br>";
+    }
+}
+
+
+
+function TiroDado (){
+    $uno = 0;
+    $dos = 0;
+    $tres = 0;
+    $cuatro = 0;
+    $cinco = 0;
+    $seis = 0;
+    for ($i=1; $i <= 10 ; $i++) {
+        $dado = rand(1, 6);
+        echo $dado;
+
+        if ($dado == 1) {
+        $uno++;
+        }
+        elseif ($dado == 2) {
+        $dos++;
+        }
+        elseif ($dado == 3) {
+        $tres++;
+        }
+        elseif ($dado == 4) {
+        $cuatro++;
+        }    
+        elseif ($dado == 5) {
+        $cinco++;
+        }
+        elseif ($dado == 6) {
+        $seis++;
+        }
+    echo $uno++;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 require_once ('functions.php');
 
+echo "EJERCICIO UNO <br>";
 ValorAleatorio(rand(1,100));
 
+echo "<br> EJERCICIO DOS";
 MesesAleatorios(rand(1,12));
+
+echo "<br> EJERCICIO TRES";
+DosValores(rand(1,100), rand(1,100));
+
+echo "<br> EJERCICIO CUATRO";
+echo "estructura For";
+PrimeraTabla (2, rand(1, 10));
+echo "<br> estructura Do-While <br>";
+Segunda (2, rand(1,10));
+echo "<br> estructura While <br>";
+Tercera (2, rand(1,10));
+
+echo "<br> EJERCICIO CINCO";
+TiroDado();
+
